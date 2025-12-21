837e3c9c92485df158412320b389a03b26e1fe1c
Ethan Simpson <ethansimpson@xtra.co.nz>
2025-12-21 19:40:08 +1300

    pathfinding

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "rlgl.h"
 #include "raymath.h"
+#include <stdlib.h>
 
 #if defined(PLATFORM_WEB)
     #define CUSTOM_MODAL_DIALOGS            // Force custom modal dialogs usage
@@ -14,6 +15,13 @@ typedef struct iVec2 {
     int x;
     int y;
 } iVec2;
+
+typedef struct Entity {
+    iVec2 tilePos;
+    Vector2 worldPos;
+    iVec2 targetTilePos;
+    float moveSpeed;
+} Entity;
 
 typedef struct RenderParams {
     int mapViewPortWidth;
@@ -42,6 +50,8 @@ typedef struct State {
     RenderTexture2D uiRendTex;
     RenderParams renderParams;
 
+    Entity PlayerEnt;
+    bool playerMoving;
     int path[200];
     int pathsize;
     int movePathIdx;
@@ -74,6 +84,316 @@ iVec2 mapWorldXYtoScreenXY(float MapX, float MapY, RenderParams param) {
     int scrollOffset = (param.smoothScrollY / param.scale);
 
     return (struct iVec2) { MapX* (float)(param.tileSize), (-MapY + tileYOffset)* (float)(param.tileSize) + mapRenderOffset % param.tileSize + scrollOffset };
+}
+
+
+int moveEntity(Entity* entity) {
+
+    //if we are already at the target return
+    if (entity->tilePos.x == entity->targetTilePos.x
+        && entity->tilePos.y == entity->targetTilePos.y) {
+        return 1;
+    }
+    Vector2 target = { entity->targetTilePos.x, entity->targetTilePos.y };
+    Vector2 dir = Vector2Normalize(Vector2Subtract(target, entity->worldPos));
+
+
+    entity->worldPos = Vector2Add(Vector2Scale(dir, entity->moveSpeed), entity->worldPos);
+
+    //entity->aniFrame += 1;
+
+    if (Vector2Distance(entity->worldPos, target) <= entity->moveSpeed) {
+        entity->tilePos = entity->targetTilePos;
+        entity->worldPos.x = entity->tilePos.x;
+        entity->worldPos.y = entity->tilePos.y;
+        //entity->aniFrames = 0;
+        return 1;
+    }
+
+    return 0;
+
+}
+
+int calcTileMoveCost(unsigned int tileData) {
+
+    int moveCost = 0;
+    return moveCost;
+}
+
+int mapXYtoIdx(unsigned char x, unsigned char y, unsigned char* mapData) {
+    unsigned char width = mapData[0];
+    return y * width + x;
+}
+
+iVec2 mapIdxToXY(int tileIdx, int mapSizeX) {
+    int x = tileIdx % mapSizeX;
+    int y = tileIdx / mapSizeX;
+    return (struct iVec2) { x, y };
+}
+
+int hDistNodes(int start, int end, int mapSizeX) {
+    iVec2 startPos = mapIdxToXY(start, mapSizeX);
+    iVec2 endPos = mapIdxToXY(end, mapSizeX);
+    return abs(endPos.x - startPos.x) + abs(endPos.y - startPos.y);
+}
+
+int getNeighbour(int tileIdx, int dir, int mapSizeX, int mapSizeY) {
+    int i = tileIdx;
+    if (dir == 0) {
+        //North
+        if (i + mapSizeX < (mapSizeX * mapSizeY)) {
+            return i + mapSizeX;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (dir == 1) {
+        //East
+        if ((i / mapSizeX) == ((i + 1) / mapSizeX)) {
+            return i + 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (dir == 2) {
+        //West
+        if ((i / mapSizeX) == ((i - 1) / mapSizeX)) {
+            return i - 1;
+        }
+        else {
+            return -1;
+        }
+
+    }
+
+    if (dir == 3) {
+        //South
+        if (i - mapSizeX >= 0) {
+            return i - mapSizeX;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    return -2;
+}
+
+int findAsPath(iVec2 startTile, iVec2 endTile, unsigned char* mapData, int* path, int pathMaxSize) {
+
+    int mapSizeX = 27;// mapData[0];
+    int mapSizeY = 19; // mapData[1];
+
+    int start = startTile.y * mapSizeX + startTile.x;
+    int end = endTile.y * mapSizeX + endTile.x;
+
+    int mapDataSizeInt = mapSizeX * mapSizeY;
+    int mapDataSizeByte = mapDataSizeInt * sizeof(int);
+    int totalDataSizeByte = mapDataSizeByte * 4;
+
+    int* aStarData = malloc(totalDataSizeByte);
+
+    int* openSet = aStarData;
+    int openSetSize = 0;
+
+    openSet[0] = start;
+    openSetSize += 1;
+
+    int* cameFromIdx = aStarData + mapDataSizeInt;
+    int* gScore = aStarData + (mapDataSizeInt * 2);
+    int* fScore = aStarData + (mapDataSizeInt * 3);
+
+    for (int i = mapDataSizeInt; i < (mapDataSizeInt * 4); i++) {
+        aStarData[i] = -1;
+    }
+
+    gScore[start] = 0;
+
+    while (openSetSize > 0) {
+
+        //Get the node with the lowest fScore
+        int curIdx = 0;
+        int fScoreLow = fScore[openSet[0]];
+
+        for (int i = 1; i < openSetSize; i++) {
+            if (fScore[openSet[i]] < fScoreLow) {
+                fScoreLow = fScore[openSet[i]];
+                curIdx = i;
+            }
+        }
+
+        if (openSet[curIdx] == end) {
+
+            int pathSize = 1;
+            int curIdx = end;
+            while (curIdx != start) {
+                curIdx = cameFromIdx[curIdx];
+                pathSize += 1;
+            }
+
+            curIdx = end;
+            int skip = pathSize - pathMaxSize;
+            if (skip < 0) {
+                skip = 0;
+            }
+            int writeIdx = pathSize - 1;
+            while (1) {
+                if (skip) {
+                    skip -= 1;
+                }
+                else {
+                    path[writeIdx] = curIdx;
+                }
+
+                if (curIdx == start) {
+                    break;
+                }
+                curIdx = cameFromIdx[curIdx];
+                writeIdx -= 1;
+            }
+
+            free(aStarData);
+
+            if (pathSize > pathMaxSize) {
+                pathSize = pathMaxSize;
+            }
+
+            return pathSize; //Return the Path we found
+        }
+
+        //check the neightbours of the current node
+        int curNode = openSet[curIdx];
+
+        //swap and drop current node
+        openSet[curIdx] = openSet[openSetSize - 1];
+        openSetSize -= 1;
+
+        for (int i = 0; i < 4; i++) {
+            int neighIdx = getNeighbour(curNode, i, mapSizeX, mapSizeY);
+            if (neighIdx == -1) {
+                continue;
+            }
+
+
+            //unsigned int dataPos = 2 + curNode * 4;
+            //unsigned int curTileData;
+            //memcpy(&curTileData, mapData + dataPos, 4);
+
+            //dataPos = 2 + neighIdx * 4;
+            //unsigned int neighTileData;
+            //memcpy(&neighTileData, mapData + dataPos, 4);
+
+            int edgeCost = 1; // (calcTileMoveCost(curTileData) + calcTileMoveCost(neighTileData)) / 2;
+
+            int tentative_gScore = gScore[curNode] + edgeCost;
+            int curNeigh_gScore = gScore[neighIdx];
+
+            if ((tentative_gScore < curNeigh_gScore) || curNeigh_gScore == -1) {
+                cameFromIdx[neighIdx] = curNode;
+                gScore[neighIdx] = tentative_gScore;
+                fScore[neighIdx] = tentative_gScore + hDistNodes(neighIdx, end, mapSizeX);
+
+                // if not in openset add
+                int add = 1;
+                for (int i = 0; i < openSetSize; i++) {
+                    if (openSet[i] == neighIdx) {
+                        add = 0;
+                    }
+                }
+
+                if (add) {
+                    openSet[openSetSize] = neighIdx;
+                    openSetSize += 1;
+                }
+            }
+
+        }
+
+    }
+
+    free(aStarData);
+    return 0;
+}
+
+void RenderPath(State* state) {
+    
+
+        //int pathRawTileCost = 0;
+
+        for (int i = state->movePathIdx; i < state->pathsize; i++) {
+
+
+
+            int nextTile = i + 1;
+            if (nextTile >= state->pathsize) nextTile = -1;
+            int prevTile = i - 1;
+            if (prevTile < 0) prevTile = -1;
+
+            int north = 0b0000000000001000;
+            int east = 0b0000000000000100;
+            int west = 0b0000000000000010;
+            int south = 0b0000000000000001;
+
+            int neighbors = 0;
+            if (nextTile > 0 && prevTile >= 0) { //drawing a path tile, not a start or end
+                if (state->path[i] + state->mapSizeX == state->path[nextTile] || state->path[i] + state->mapSizeX == state->path[prevTile]) { //tile to the north
+                    neighbors |= north;
+                }
+                if (state->path[i] + 1 == state->path[nextTile] || state->path[i] + 1 == state->path[prevTile]) { //tile to the east
+                    neighbors |= east;
+                }
+                if (state->path[i] - 1 == state->path[nextTile] || state->path[i] - 1 == state->path[prevTile]) { //tile to the west
+                    neighbors |= west;
+                }
+                if (state->path[i] - state->mapSizeX == state->path[nextTile] || state->path[i] - state->mapSizeX == state->path[prevTile]) { //tile to the south
+                    neighbors |= south;
+                }
+            }
+
+            int drawtileID = 0; //start/end tile
+
+            if (neighbors == (north | south)) {
+                drawtileID = 1;
+            }
+            else if (neighbors == (east | west)) {
+                drawtileID = 2;
+            }
+            else if (neighbors == (north | west)) {
+                drawtileID = 3;
+            }
+            else if (neighbors == (south | east)) {
+                drawtileID = 4;
+            }
+            else if (neighbors == (south | west)) {
+                drawtileID = 5;
+            }
+            else if (neighbors == (north | east)) {
+                drawtileID = 6;
+            }
+            else {
+                drawtileID = -1;
+            }
+
+            Rectangle pathTileSrc = { drawtileID * state->tileSize, 2 * state->tileSize, state->tileSize,state->tileSize };
+
+            iVec2 tileLoc = mapIdxToXY(state->path[i], state->mapSizeX);
+            iVec2 pathPos = mapTileXYtoScreenXY(tileLoc.x, tileLoc.y, state->renderParams);
+
+            //unsigned int tileData = getTileData(tileLoc.x, tileLoc.y, state->mapData);
+            //int tileMoveCost = calcTileMoveCost(tileData);
+
+            //pathRawTileCost += tileMoveCost;
+
+
+            DrawTextureRec(state->ui, pathTileSrc, (struct Vector2) { pathPos.x, pathPos.y }, WHITE);
+            
+
+        }
+    
 }
 
 void UpdateDrawFrame(void* v_state) {
@@ -120,6 +440,17 @@ void UpdateDrawFrame(void* v_state) {
     state->renderParams.smoothScrollY = state->smoothScrollY;
 
 
+    state->pathsize = findAsPath(state->PlayerEnt.tilePos, state->cursTilePos, state->mapData, state->path, 200);
+    state->totalPathCost = 0;
+    for (int i = 0; i < state->pathsize; i++) {
+        //unsigned int dataPos = 2 + state->path[i] * 4;
+        //unsigned int tileData;
+        //memcpy(&tileData, state->mapData + dataPos, 4);
+        //state->totalPathCost += calcTileMoveCost(tileData);
+        state->totalPathCost += 1;
+    }
+
+
     float col_length = 1.0f;
     
     BeginDrawing();
@@ -143,6 +474,10 @@ void UpdateDrawFrame(void* v_state) {
         iVec2 cursPixel = mapTileXYtoScreenXY(state->cursTilePos.x, state->cursTilePos.y, state->renderParams);
 
         DrawTextureRec(state->ui, cursorRec, (struct Vector2) { cursPixel.x - centOff, cursPixel.y - centOff }, WHITE);
+
+        if (state->pathsize) {
+            RenderPath(state);
+        }
 
     EndTextureMode();
 
@@ -184,6 +519,10 @@ int main(void)
     state.screenHeight = state.baseSizeY * state.scale - state.scale;
 
     state.cursTilePos = (iVec2){ 13,8 };
+    state.PlayerEnt = (Entity){ (struct iVec2) { 13,8 },(struct Vector2) { 13.0f,8.0f },(struct iVec2) { 13,8 }, 0.01f };
+    state.pathsize = 0;
+    state.movePathIdx = 0;
+    state.totalPathCost = 0;
 
     state.mapData = LoadFileData("resources/respitetest.rspb", &state.mapDataSize);
     state.mapSizeX = 27;
@@ -202,6 +541,8 @@ int main(void)
     state.uiRendTex = LoadRenderTexture(state.baseSizeX, state.baseSizeY);
    
     state.renderParams = (RenderParams){ state.baseSizeX ,state.baseSizeY,state.map.width,state.map.height,state.tileSize,state.smoothScrollY,state.scale };
+
+    state.playerMoving = false;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
